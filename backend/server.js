b1e7a740afb861d9cdc2c02d10bf6d037d12d227
kooroshFarv <farvardin.koorosh@gmail.com>
2026-02-18 23:10:20 -0500

    server port

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
 const createApp = require("./src/app");
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT ;
 
 const app = createApp();
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-  console.log(`Health check: http://localhost:${PORT}/health`);
 });
