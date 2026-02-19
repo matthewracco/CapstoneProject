455c3fe68a148899d1f6e68ddd5870f0d1bd408c
kooroshFarv <farvardin.koorosh@gmail.com>
2026-02-18 23:30:28 -0500

    final touch

--- backend/server.js
+++ backend/server.js
@@ -6,5 +6,5 @@ const app = createApp();
 const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening port ${PORT}`);
 });
